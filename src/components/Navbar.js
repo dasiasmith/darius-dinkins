93af4e08351ad0c7934d178b4890f7edb1f82c14
Dasia Smith <dasiasmith@Dasias-MacBook-Air.local>
2023-05-07 17:35:03 -0600

    Updated header and bio

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,43 @@
 import { ArrowRightIcon } from "@heroicons/react/solid";
 import React from "react";
+import { AiOutlineInstagram, AiOutlineYoutube } from 'react-icons/ai';
+import {TikTokIcon} from './Tiktok';
 
 export default function Navbar() {
   return (
-    <header className="bg-gray-800 md:sticky top-0 z-10">
+    <header className="bg-transparent md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <a className="title-font font-medium text-white mb-4 md:mb-0">
-          <a href="#about" className="ml-3 text-xl">
-            Reed Barger
+          <a href="#about" className="ml-3 text-xl nav-darius">
+            Darius Dinkins
           </a>
         </a>
-        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-          <a href="#projects" className="mr-5 hover:text-white">
-            Past Work
+        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4	flex flex-wrap items-center text-base justify-center">
+          <a href="#about" className="mr-5 hover:text-gray-700 nav-sec">
+            About
           </a>
-          <a href="#skills" className="mr-5 hover:text-white">
-            Skills
+          <a href="#shows" className="mr-5 hover:text-gray-700 nav-sec">
+            Shows
           </a>
-          <a href="#testimonials" className="mr-5 hover:text-white">
-            Testimonials
+          <a href="#press" className="mr-5 hover:text-gray-700 nav-sec">
+            Press
           </a>
+          <a href="https://www.youtube.com/@theotherotherdarius" target="_blank" rel="noopener noreferrer" className="mr-5 hover:text-gray-700 nav-sec">
+            <AiOutlineYoutube/>
+          </a>
+          <a href="https://www.instagram.com/theotherotherdarius/" target="_blank" rel="noopener noreferrer" className="mr-5 hover:text-gray-700 nav-sec">
+            <AiOutlineInstagram/>
+          </a>
+          <a href="https://www.tiktok.com/@theotherotherdarius" target="_blank" rel="noopener noreferrer" className="mr-5 hover:text-gray-700">
+            <TikTokIcon/>
+          </a>  
         </nav>
         <a
           href="#contact"
-          className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
+          className="inline-flex items-center bg-transparent border-0 py-1 px-3 focus:outline-none text-white hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
         >
-          Hire Me
-          <ArrowRightIcon className="w-4 h-4 ml-1" />
+          Book Me?
+          <ArrowRightIcon className="w-4 h-4 ml-1 book-me" />
         </a>
       </div>
     </header>
